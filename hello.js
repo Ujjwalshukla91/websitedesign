cb9a66237c7d75dd290532fa2fde49cd14278c66
Ujjwalshukla91 <ujjwalshukla245@gmail.com>
2021-01-23 03:22:57 +0530

    js odd even
    js odd even

--- hello.js
+++ hello.js
@@ -1,3 +1,11 @@
-function(){
+function form(){
+    var fname =document.getElementById("fname");
+    var pass = document.getElementById("pass");
+    
+}
 
+for(var a = 1; < = 10; a++){
+    if(a % 2 == 0){
+        document.write(a + "<br>");
+    }
 }
